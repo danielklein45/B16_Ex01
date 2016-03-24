77d4d5cbc5e7ec3c085097e8c1aeddf06e478f71
daniel klein <danielklein45@gmail.com>
2016-03-24 16:39:16 +0200

    FirstCommit

---
+++ B16 Ex01 Daniel 301344891 Mike 310675582/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B16_Ex01_Daniel_301344891_Mike_310675582
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
