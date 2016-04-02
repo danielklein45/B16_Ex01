fbaddc00bc13d12c22b6de0074409dba5ad5e747
MatrixDevil <mike.finkelson@gmail.com>
2016-04-02 19:44:23 +0300

    Almost Done the Top Photos, still need to be done with the creation of Photos panel and under thread. This is mostly the final design.

---
+++ B16 Ex01 Daniel 301344891 Mike 310675582/PopularPanelMgt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FacebookSmartView
+{
+    class PopularPanelMgt
+    {
+        private const int MAX_PANEL = 4;
+        private static Point m_lastEndOfPicture;
+
+        private List<PictureObject> m_LstPictureBoxFromForm;
+        public PopularPanelMgt()
+        {
+            m_LstPictureBoxFromForm = new List<PictureObject>();
+            m_lastEndOfPicture = new Point(0, 0);
+        }
+
+        public List<PictureObject> PictureObjectList
+        {
+            get
+            {
+                return m_LstPictureBoxFromForm; 
+            }
+        }
+
+        public bool tryAddToPanel(SpecialPictureBox i_poNew)
+        {
+            if (m_LstPictureBoxFromForm.Count < MAX_PANEL)
+            {
+                m_LstPictureBoxFromForm.Add(new PictureObject("", 0, 0, 0, "", i_poNew, i_poNew.PicLabel));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool tryAddToPanel(PictureObject i_poNew)
+        {
+            if (m_LstPictureBoxFromForm.Count < MAX_PANEL)
+            {
+                m_LstPictureBoxFromForm.Add(i_poNew);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool getNextFreeLocationInContainer(Point i_MaxPoint, out Point o_NewPoint)
+        {
+            if (m_lastEndOfPicture.X < i_MaxPoint.X && m_lastEndOfPicture.Y < i_MaxPoint.Y)
+            {
+                o_NewPoint = new Point(m_lastEndOfPicture.X + 10, m_lastEndOfPicture.Y);
+                return true;
+            }
+            else
+            {
+                o_NewPoint = new Point(0, 0);
+                return false;
+            }
+                
+        }
+    }
+}
---
+++ B16 Ex01 Daniel 301344891 Mike 310675582/SpecialPictureBox.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FacebookSmartView
+{
+    partial class SpecialPictureBox : PictureBox
+    {
+        private Panel m_FatherPanel;
+        private Panel m_boxPanel;
+        private Label m_botLabel;
+
+        public SpecialPictureBox(Panel i_FatherPanel)
+        {
+            //Point newPointForPanel;
+            //bool bIsThereFreeSpace = false;
+            m_FatherPanel = i_FatherPanel;
+
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Size = new Size(180, 140);
+            this.Location = new Point(0, 0);
+            this.Name = m_FatherPanel.ToString() + "_PicBox";
+
+
+            m_botLabel = new Label();
+            m_botLabel.Size = new Size(72, 15);
+            m_botLabel.Location = new Point(5, 146);
+            m_botLabel.AutoSize = true;
+
+            this.Visible = true;
+
+            //m_boxPanel = new Panel();
+            //m_boxPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            //m_boxPanel.Size = new Size(180, 165);
+            //bIsThereFreeSpace = PopularPanelMgt.getNextFreeLocationInContainer(new Point(m_boxPanel.Size.Width, m boxPanel.Size.Height), newPointForPanel);
+            //m_boxPanel.Location = new Point(newPointForPanel);
+
+
+            m_FatherPanel.Controls.Add(this);
+            m_FatherPanel.Controls.Add(m_botLabel);
+
+            //m_FatherPanel.Controls.Add(m_boxPanel);
+        }
+
+        public Panel FatherPanel { get { return m_FatherPanel; } }
+        public Label PicLabel { get { return m_botLabel; } }
+
+        protected override void OnMouseHover(EventArgs e)
+        {
+            m_botLabel.BackColor = Color.LightSkyBlue;
+            base.OnMouseHover(e);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            m_botLabel.BackColor = SystemColors.ButtonHighlight;
+            base.OnMouseLeave(e);
+        }
+        
+    }
+}
