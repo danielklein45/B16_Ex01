e7b532ffd84944ccaf0f70e6e5c314c11fdb9adf
daniel klein <danielklein45@gmail.com>
2016-04-06 00:31:36 +0300

    test

--- B16 Ex01 Daniel 301344891 Mike 310675582/LoginLogic.cs
+++ B16 Ex01 Daniel 301344891 Mike 310675582/LoginLogic.cs
@@ -9,11 +9,11 @@ using FacebookWrapper.ObjectModel;
 
 namespace FacebookSmartView
 {
-    class LoginLogic
-    {
-        // Private Variables
-        private const string k_AppID = "1222488951112730";
-        private readonly string[] r_RequiredPermissions = 
+	class LoginLogic
+	{
+		// Private Variables
+		private const string k_AppID = "1222488951112730";
+		private readonly string[] r_RequiredPermissions = 
 		{
 			"user_about_me",
 			"user_posts",
@@ -26,49 +26,50 @@ namespace FacebookSmartView
 			"user_photos",
 			"user_posts",
 			"user_relationships"
+            //daniel klein
 		};
 
-        // Public Functions
+		// Public Functions
 
-        public string getSavedAccessToken()
-        {
-            return Properties.Settings.Default.SavedAccessToken;
-        }
+		public string getSavedAccessToken()
+		{
+			return Properties.Settings.Default.SavedAccessToken;
+		}
 
-        public void saveAccessToken(string i_AccessToken)
-        {
-            Properties.Settings.Default.SavedAccessToken = i_AccessToken;
-            Properties.Settings.Default.Save();
-        }
+		public void saveAccessToken(string i_AccessToken)
+		{
+			Properties.Settings.Default.SavedAccessToken = i_AccessToken;
+			Properties.Settings.Default.Save();
+		}
 
-        public LoginResult loginToFacebook()
-        {
-            return FacebookService.Login(k_AppID, r_RequiredPermissions);
-        }
+		public LoginResult loginToFacebook()
+		{
+			return FacebookService.Login(k_AppID, r_RequiredPermissions);
+		}
 
-        public LoginResult loginToFacbookAndSaveToken()
-        {
-            LoginResult result = loginToFacebook();
-            saveAccessToken(result.AccessToken);
+		public LoginResult loginToFacbookAndSaveToken()
+		{
+			LoginResult result = loginToFacebook();
+			saveAccessToken(result.AccessToken);
 
-            return result;
-        }
+			return result;
+		}
 
-        public LoginResult connectToFacebook(string i_LastAccToken)
-        {
-            return FacebookService.Connect(i_LastAccToken);
-        }
+		public LoginResult connectToFacebook(string i_LastAccToken)
+		{
+			return FacebookService.Connect(i_LastAccToken);
+		}
 
-        public void saveRememberBox(bool i_bCheckBoxValue)
-        {
-            Properties.Settings.Default.RememberMe = i_bCheckBoxValue;
-            Properties.Settings.Default.Save();
-        }
+		public void saveRememberBox(bool i_bCheckBoxValue)
+		{
+			Properties.Settings.Default.RememberMe = i_bCheckBoxValue;
+			Properties.Settings.Default.Save();
+		}
 
-        public bool getRememberBoxCheckedValue()
-        {
-            return Properties.Settings.Default.RememberMe;
-        }
-    }
+		public bool getRememberBoxCheckedValue()
+		{
+			return Properties.Settings.Default.RememberMe;
+		}
+	}
 
 }
